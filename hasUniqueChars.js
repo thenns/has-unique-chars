f16731ccc5b12fbbefd29a02729f3b65be3edbb9
thenns <samm77@gmail.com>
2022-09-03 15:12:46 -0600

    finished challenge

---
+++ hasUniqueChars.js
@@ -0,0 +1,13 @@
+// Write your code below
+function hasUniqueChars (str) {
+    for (let i = 0; i < str.length; i++) {
+        for (let j = i + 1; j < str.length; j++) {
+            if (str[i] === str[j]){
+                return false;
+                break;
+            }
+        }
+    }
+    return true;
+}
+console.log(hasUniqueChars ("abcedefaaaaa"));
